4e7e591490c52c2f8180dfe82b39d5bc1288ab20
bkc <bkcgee@gmail.com>
2021-06-25 19:27:19 -0500

    added body-parser to package

--- src/server/configure.js
+++ src/server/configure.js
@@ -1,7 +1,11 @@
 const bodyParser = require('body-parser')
+// const express = require('express')
+
 const api = require('./api')
 
+
 module.exports = app => {
+  // app.use(express.json())
   app.use(bodyParser.json())
   app.use('/api', api)
 }
